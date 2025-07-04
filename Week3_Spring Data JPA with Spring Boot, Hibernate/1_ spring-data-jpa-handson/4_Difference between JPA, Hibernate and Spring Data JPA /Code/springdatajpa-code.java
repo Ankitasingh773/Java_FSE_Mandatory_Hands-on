1b41078ddc3022ca1328853bf876a378066f101e
Ankitasingh773 <singhankitakumari463@gmail.com>
2025-07-04 16:33:37 +0530

    Create springdatajpa-code.java

---
+++ Week3_Spring Data JPA with Spring Boot, Hibernate/1_ spring-data-jpa-handson/4_Difference between JPA, Hibernate and Spring Data JPA /Code/springdatajpa-code.java
@@ -0,0 +1,13 @@
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+}
