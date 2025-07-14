0a95ddcb1f76c5d3a066ade93339422f74974fae
Ankitasingh773 <singhankitakumari463@gmail.com>
2025-07-14 07:15:55 +0530

    Create JwtUtil.java

---
+++ Week4_Spring REST using Spring Boot 3/5_JWT-handson/1_Create authentication service that returns JWT/Code/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.example.jwt.auth.util;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String secret = "mysecretkey"; 
+    private final long expirationTimeMs = 1000 * 60 * 10; 
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTimeMs))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
